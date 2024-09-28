40c05ede494253bc1e0ebd7631956a955b90f205
ameliaaaaa <11111f11@123.com>
2024-09-28 14:05:23 +0800

    fixed 2 bugs: 1. toggle button required js

---
+++ Project8/static/script.js
@@ -0,0 +1,9 @@
+function toggleHiragana(button) {
+    const hiraganaSpan = button.previousElementSibling; // Get the span before the button
+    hiraganaSpan.classList.toggle('hidden');
+    }
+
+function toggleMeaning(button) {
+    const meaningSpan = button.previousElementSibling;
+    meaningSpan.classList.toggle('hidden');
+}
